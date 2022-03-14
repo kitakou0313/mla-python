13faf7f6be7482e465103ee784dc5a8f8c6db1f5
kitakou0313 <kitakou0313@gmail.com>
2022-03-14 13:55:29 +0000

    Add bone sk model

---
+++ model/linear/linear.py
@@ -0,0 +1,24 @@
+
+
+class LinearRegression(object):
+    """
+    線形回帰
+    """
+
+    def __init__(self, parameter_list):
+        """
+        コンストラクタ
+        """
+        pass
+
+    def fit(self, parameter_list):
+        """
+        学習
+        """
+        pass
+
+    def predict(self, parameter_list):
+        """
+        推論
+        """
+        pass
