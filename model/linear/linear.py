03c895545793999d288297038213a4eef04e0685
kitakou0313 <kitakou0313@gmail.com>
2022-03-16 08:09:59 +0000

    Define linear reg

--- model/linear/linear.py
+++ model/linear/linear.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 
 class LinearRegression(object):
@@ -5,20 +6,42 @@ class LinearRegression(object):
     線形回帰
     """
 
-    def __init__(self, parameter_list):
+    def __init__(self, D: int):
         """
         コンストラクタ
+        D: int 入力次元
         """
-        pass
+        self.w = np.zeros(D)
+        self.D = D
 
-    def fit(self, parameter_list):
+    def fit(self, iters: int, alpha: np.float64, x: np.ndarray, yt: np.ndarray):
         """
         学習
         """
-        pass
+        M = x.shape[0]
+        D = self.D
 
-    def predict(self, parameter_list):
+        history = np.array([])
+
+        for k in range(1, iters+1):
+            yp = self.predict(x)
+            yd = yp-yt
+
+            self.w = self.w - alpha / M * (x.T @ yd)
+
+            if (k % 100 == 0):
+                # 損失関数値の計算 (7.6.1)
+                loss = np.mean(yd ** 2) / 2
+                # 計算結果の記録
+                history = np.append(history, loss)
+                # 画面表示
+                print("iter = %d  loss = %f" % (k, loss))
+
+        print("Loss init", history[0])
+        print("Final Loss", history[-1])
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
         """
         推論
         """
-        pass
+        return (x @ self.w)
