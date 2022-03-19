540c0932734c42db4bad23eac6b27313294d1744
kitakou0313 <kitakou0313@gmail.com>
2022-03-19 03:33:26 +0000

    Defile softmax and multi cross entropy

--- model/utils/__init__.py
+++ model/utils/__init__.py
@@ -14,3 +14,22 @@ def cross_entropy(yt: np.ndarray, yp: np.ndarray) -> np.float64:
     """
     ce = -(yt * np.log(yp) + (1-yt)*(1-yp))
     return np.mean(ce)
+
+
+def multi_cross_entropy(yt: np.ndarray, yp: np.ndarray) -> np.float64:
+    """
+    多クラス分類用のクロスエントロピー
+    """
+    return -np.mean(np.sum(yt * np.log(yp), axis=1))
+
+
+def softmax(x: np.ndarray) -> np.ndarray:
+    """
+    ソフトマックス
+    """
+    x = x.T
+    x_max = x.max(axis=0)
+    x = x - x_max
+    w = np.exp(x)
+
+    return (w / w.sum(axis=0)).T
