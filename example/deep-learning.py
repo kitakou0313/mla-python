d4801d545b132be998ea3fc8bbdf55b4ba199b88
kitakou0313 <kitakou0313@gmail.com>
2022-03-21 13:54:28 +0000

    Add data proc

--- example/deep-learning.py
+++ example/deep-learning.py
@@ -1,6 +1,8 @@
 import torchvision
 import numpy as np
 
+from sklearn.preprocessing import OneHotEncoder
+
 
 def classificate_mnist():
     """
@@ -13,9 +15,18 @@ def classificate_mnist():
 
     train: np.ndarray = train_tensor.data.numpy()
     train = train.reshape([train.shape[0], -1])
+    train = np.insert(train, 0, 1, axis=1)
+    train = train / 255.0
     train_target: np.ndarray = train_tensor.targets.numpy()
+
+    ohe = OneHotEncoder(sparse=False)
+    train_target_onehot = ohe.fit_transform(np.c_[train_target])
+    print('One Hot Vector化後', train_target_onehot.shape)
+
     test: np.ndarray = test_tensor.data.numpy()
     test = test.reshape([test.shape[0], -1])
+    test = np.insert(test, 0, 1, axis=1)
+    test = test / 255.0
     test_taeget: np.ndarray = test_tensor.targets.numpy()
 
     pass
